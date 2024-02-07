78400b79bfcb231d3ede41a12eb0c0a823141e1c
sHovonIslamx23 <saifulislamshovon056@gmail.com>
2024-02-06 23:43:09 -0800

    Signup Page First Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        whitesmoke: {
+          "200": "#eee",
+        },
+      },
+      spacing: {},
+      },
+    
   },
-  plugins: [],
-}
-
+  corePlugins: {
+    preflight: false,
+  },
+};
