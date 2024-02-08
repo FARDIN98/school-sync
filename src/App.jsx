85188cd4f343b3e57737531d85d6ef5097817656
FARDIN98 <fardinahmed66@gmail.com>
2024-02-08 22:38:36 +0600

    more changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-import React from 'react';
+
 import { BrowserRouter , Route, Routes } from 'react-router-dom'; // Import Router, Route, Switch from react-router-dom
-import { useState } from 'react';
 import './App.css';
 import Signup from './components/Signup/Signup';
 import Login from './components/Login/Login';
 import LandingPage from './components/LandingPage/LandingPage';
 function App() {
-  const [count, setCount] = useState(0);
+  
 
   return (
      <BrowserRouter>
      <Routes>
-     <Route path="/" element={<Signup />} />
+     <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/home" element={<LandingPage />} />
+        <Route path="/home" element={<Signup />} />
      </Routes>
      
      
