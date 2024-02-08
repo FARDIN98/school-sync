bfa40d6c099825a050fef3f6264e91d60cfcdda9
sHovonIslamx23 <saifulislamshovon056@gmail.com>
2024-02-07 23:48:22 -0800

    Basic Login Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
-import { useState } from 'react'
-import './App.css'
-import Signup from './components/Signup/Signup'
-
+import React from 'react';
+import { BrowserRouter , Route, Routes } from 'react-router-dom'; // Import Router, Route, Switch from react-router-dom
+import { useState } from 'react';
+import './App.css';
+import Signup from './components/Signup/Signup';
+import Login from './components/Login/Login';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <Signup/>
-    
-  )
+     <BrowserRouter>
+     <Routes>
+     <Route path="/" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+     </Routes>
+     
+     
+     
+     
+     
+     </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,47 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        primary: {
+          "50": "#eff6ff",
+          "100": "#dbeafe",
+          "200": "#bfdbfe",
+          "300": "#93c5fd",
+          "400": "#60a5fa",
+          "500": "#3b82f6",
+          "600": "#2563eb",
+          "700": "#1d4ed8",
+          "800": "#1e40af",
+          "900": "#1e3a8a",
+          "950": "#172554"
+        },
         whitesmoke: {
+          "100": "#f4f4f4",
           "200": "#eee",
         },
+        black: "#000",
+        red: "#fc0000",
+        midnightblue: "#1e2772",
+        silver: "#c2c2c2",
+        white: "#fff",
+        deepskyblue: "#3da8e4",
+        dimgray: "#555",
       },
       spacing: {},
+      fontFamily: {
+        "istok-web": "'Istok Web'",
+        poppins: "Poppins",
       },
-    
+      borderRadius: {
+        "6xl": "25px",
+        "4xs-1": "8.1px",
+      },
+      fontSize: {
+        "13xl": "32px",
+        "base-2": "16.2px",
+        "mini-1": "14.1px",
+        inherit: "inherit",
+      },
+    },
   },
   corePlugins: {
     preflight: false,
