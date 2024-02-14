4d3536702baee16c4c2f3c715d857b4cb1bd7bfc
FARDIN98 <fardinahmed66@gmail.com>
2024-02-14 22:03:47 +0600

    some changes in landingPage

--- src/components/LandingPage/Footer/Footer.jsx
+++ src/components/LandingPage/Footer/Footer.jsx
@@ -7,6 +7,7 @@ const Footer = () => {
 <footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
     <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
+        
             <a  className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
                 
                 <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">School-Sync</span>
