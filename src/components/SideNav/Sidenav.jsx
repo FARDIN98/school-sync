cafd91de47cbb716a342edc54bbf80eb0f4c00b4
sHovonIslamx23 <saifulislamshovon056@gmail.com>
2024-02-19 07:34:35 -0800

    Sidenav files added

---
+++ src/components/SideNav/Sidenav.jsx
@@ -0,0 +1,57 @@
+import React, { Children } from 'react';
+import logo from "../../assets/images/Group.png";
+import { HiOutlineViewGrid,HiTrendingUp,HiOutlinePencilAlt,HiOutlineNewspaper,HiQuestionMarkCircle,HiOutlineCog,HiLogout } from 'react-icons/hi';
+import { HiCalendarDays,HiDocumentText,HiMiniUsers,HiComputerDesktop } from "react-icons/hi2";
+import { LuShieldQuestion } from "react-icons/lu";
+
+export default function Sidenav() {
+  return (
+    <aside className='h-screen'>
+      <nav className='h-full flex flex-col bg-white border-r shadow-sm'>
+        <div className='p-4 pb-2 flex justify-between items-center'>
+          <img src={logo} className='w-32' alt="" />
+        </div>
+        <div className="flex flex-col flex-grow p-4 w-screen">
+        <div className="mb-4">
+        <a href="#" className="flex items-center py-2 px-2 rounded-lg text-blue-800  bg-gradient-to-b from white to white hover:from-blue-500 hover:to-white-300 no-underline font-medium">
+            <HiOutlineViewGrid className="mr-2 w-7 h-7" /> Dashboard
+          </a> 
+          <a href="#" className="flex items-center py-2 px-2 rounded-lg text-blue-800  bg-gradient-to-b from white to white hover:from-blue-500 hover:to-white-300 no-underline font-medium mt-2">
+            <HiTrendingUp className="mr-2 w-7 h-7" /> Progress Tracking
+          </a>
+          <a href="#" className="flex items-center py-2 px-2 rounded-lg text-blue-800  bg-gradient-to-b from white to white hover:from-blue-500 hover:to-white-300 no-underline font-medium mt-2">
+            <HiOutlinePencilAlt className="mr-2 w-7 h-7" /> Exams
+          </a>
+          <a href="#" className="flex items-center py-2 px-2 rounded-lg text-blue-800  bg-gradient-to-b from white to white hover:from-blue-500 hover:to-white-300 no-underline font-medium mt-2">
+            <HiCalendarDays className="mr-2 w-7 h-7" /> Academic Calender
+          </a>
+          <a href="#" className="flex items-center py-2 px-2 rounded-lg text-blue-800  bg-gradient-to-b from white to white hover:from-blue-500 hover:to-white-300 no-underline font-medium mt-2">
+            <HiOutlineNewspaper className="mr-2 w-7 h-7" /> Courses
+          </a>
+          <a href="#" className="flex items-center py-2 px-2 rounded-lg text-blue-800  bg-gradient-to-b from white to white hover:from-blue-500 hover:to-white-300 no-underline font-medium mt-2">
+            <HiQuestionMarkCircle className="mr-2 w-7 h-7" /> Suggestions
+          </a>
+          <a href="#" className="flex items-center py-2 px-2 rounded-lg text-blue-800  bg-gradient-to-b from white to white hover:from-blue-500 hover:to-white-300 no-underline font-medium mt-2">
+            <HiDocumentText className="mr-2 w-7 h-7" /> Assignments
+          </a>
+          <a href="#" className="flex items-center py-2 px-2 rounded-lg text-blue-800  bg-gradient-to-b from white to white hover:from-blue-500 hover:to-white-300 no-underline font-medium mt-2">
+            <HiMiniUsers className="mr-2 w-7 h-7" /> Attendance
+          </a>
+          <a href="#" className="flex items-center py-2 px-2 rounded-lg text-blue-800  bg-gradient-to-b from white to white hover:from-blue-500 hover:to-white-300 no-underline font-medium mt-2">
+            <HiComputerDesktop className="mr-2 w-7 h-7" /> AI Chatbot
+          </a>
+          <a href="#" className="flex items-center py-2 px-2 rounded-lg text-blue-800  bg-gradient-to-b from white to white hover:from-blue-500 hover:to-white-300 no-underline font-medium mt-2">
+            <LuShieldQuestion className="mr-2 w-7 h-7" /> Help
+          </a>
+          <a href="#" className="flex items-center py-2 px-2 rounded-lg text-blue-800  bg-gradient-to-b from white to white hover:from-blue-500 hover:to-white-300 no-underline font-medium mt-2">
+            <HiOutlineCog className="mr-2 w-7 h-7" /> Settings
+          </a>
+          <a href="#" className="flex items-center py-2 px-2 rounded-lg text-blue-800  bg-gradient-to-b from white to white hover:from-blue-500 hover:to-white-300 no-underline font-medium mt-2">
+            <HiLogout className="mr-2 w-7 h-7" /> Logout
+          </a>
+        </div>
+      </div>
+      </nav>
+    </aside>
+  );
+}
